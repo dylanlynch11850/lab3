7eaabeb17610e9ea86485ae4d5c1bddd0a4665a1
dylanlynch11850 <55436281+dylanlynch11850@users.noreply.github.com>
2022-10-11 14:35:39 +0100

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -4,9 +4,12 @@ import { BookItem } from "./bookItem";
 export class Books extends React.Component{
     render(){
 
+        //This is array of functions 
         //collect data 
 //This will get rid of red warning 
         return this.props.books.map( (book)=>{
+            
+            //Create a new item called book item
             return <BookItem book={book} key={book.isbn}></BookItem>
         }
 
